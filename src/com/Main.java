97c17fc93f16191404e60cb3be852b943440fef2
ivan4000 <sxln1@mail.ru>
2018-10-31 22:53:12 +0300

    JL72concurrency

---
+++ src/com/Main.java
@@ -0,0 +1,46 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NameList nameList = new NameList();
+        nameList.add("first");
+        class MyThread extends Thread {
+            @Override
+            public void run() {
+                System.out.println(nameList.removeFirst());
+            }
+        }
+        MyThread myThread = new MyThread();
+        myThread.setName("one");
+        myThread.start();
+        new MyThread().start();
+    }
+
+    static class NameList {
+        List list = Collections.synchronizedList(new ArrayList<>());
+
+        public synchronized void add(String name) {
+            list.add(name);
+        }
+
+        public List getList() {
+            return list;
+        }
+
+
+        public synchronized String removeFirst() {
+            if (list.size() > 0) {
+                if (Thread.currentThread().getName().equals("one")) ;
+                Thread.yield();
+                return (String) list.remove(0);
+            }
+            return null;
+        }
+    }
+}
